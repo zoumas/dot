a7a1a70e1cfe67e17f3c425f534ee1641a914ad2
zoumas <zoumas@protonmail.com>
2024-06-25 17:58:16 +0300

    update with gopls custom file and gruvbox theme

---
+++ nvim/lua/plugins/gopls.lua
@@ -0,0 +1,61 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            gofumpt = true,
+            codelenses = {
+              gc_details = true,
+              test = true,
+              run_govulncheck = true,
+
+              unreachable = true,
+              unusedvariable = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            analyses = {
+              fieldalignment = true,
+              shadow = true,
+              useany = true,
+            },
+            usePlaceholders = true,
+            staticcheck = true,
+            completeUnimported = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
+          },
+        },
+      },
+    },
+    setup = {
+      gopls = function(_, opts)
+        -- workaround for gopls not supporting semanticTokensProvider
+        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+        LazyVim.lsp.on_attach(function(client, _)
+          if not client.server_capabilities.semanticTokensProvider then
+            local semantic = client.config.capabilities.textDocument.semanticTokens
+            client.server_capabilities.semanticTokensProvider = {
+              full = true,
+              legend = {
+                tokenTypes = semantic.tokenTypes,
+                tokenModifiers = semantic.tokenModifiers,
+              },
+              range = true,
+            }
+          end
+        end, "gopls")
+        -- end workaround
+      end,
+    },
+  },
+}
