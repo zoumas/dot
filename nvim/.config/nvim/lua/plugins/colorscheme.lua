70d907ee5b4a8a3222150e705b94d7ef57b3b3a9
zoumas <zoumas@protonmail.com>
2026-08-30 17:39:58 +0300

    Show hidden files in snacks picker/explorer, switch theme to Gruvbox Material
    - Set picker.sources.files.hidden and picker.sources.explorer.hidden
      to true so both find-files and the file explorer show dotfiles by
      default (gitignored files are still hidden)
    - Switch ghostty and neovim to Gruvbox Material (medium contrast,
      dark): ghostty theme = Gruvbox Material Dark, neovim uses
      sainnhe/gruvbox-material with background=medium

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 return {
-  { "folke/tokyonight.nvim", opts = { style = "storm" } },
-  { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.gruvbox_material_background = "medium"
+    end,
+  },
+  { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox-material" } },
 }
